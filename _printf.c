80198ab099ff0eb89133c4ce599400410b306573
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-18 20:28:02 +0300

    _printf

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include<stdarg.h>
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * _printf - The  functions  in  the _printf() family 
+ * produce output according to a format
+ * Return: these functions return the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+    
+}
