25e4009afd85691099230ffad39aa51428ddfd15
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-19 23:55:35 +0300

    hotfix

--- _printf.c
+++ _printf.c
@@ -3,21 +3,23 @@
 #include "main.h"
 
 /**
- * _printf - The  functions  in  the _printf() family 
+ * _printf - The  functions  in  the _printf() family
  * produce output according to a format
- * Return: these functions return the number of characters printed
+ * Return: these functions return
+ * the number of characters printed
+ * @format: a character string
  */
 
 int _printf(const char *format, ...)
 {
     int count;
-	int total = 0;
+    int total = 0;
 	va_list args;
 	int flag = 0;
 
 	if (format == NULL)
 		return (0);
-
+        
 	va_start(args, format);
 	for (count = 0; *(format + count) != '\0'; count++)
 	{
--- main.h
+++ main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define NULL ((void *)0)
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _print_str(char *string);
+
+#endif
