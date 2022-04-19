02dfd8180259527db0eaeb4565ac4161de542c85
Atsnagnkifle <kifleatsnagn@gmail.com>
2022-04-20 00:10:43 +0300

    printf 1 handle d i

--- print_func.c
+++ print_func.c
@@ -2,6 +2,32 @@
 #include<stddef.h>
 
 /**
+ * _print_int - prints an integer
+ * @var: variable to be printed
+ * Return: length of printed characters
+ */
+
+int _print_int(long int var)
+{
+	int count = 0;
+
+	if (var < 0)
+	{
+		_putchar('-');
+		count += 1;
+		var = -var;
+	}
+
+	if (var / 10)
+		count += _print_int(var / 10);
+
+	_putchar(var % 10 + '0');
+	count += 1;
+
+	return (count);
+}
+
+/**
  * _print_str - prints a string
  * @string: string to be printed
  * Return: length of printed characters
